8a7571c748f4167da3830b3d7f2492622376e0b9
ezgiozbudak <96118303+ezgiozbudak@users.noreply.github.com>
2021-12-18 22:38:17 +0300

    Console Programlama dosyası eklendi

---
+++ console programlama/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsminizi Girin:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyisminizi Girin:");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Hello " + name + " " + surname);
+        }
+    }
+}
