3673f286a245ac924069fc2640db68cf96f7a9e3
ezgiozbudak <96118303+ezgiozbudak@users.noreply.github.com>
2022-04-07 00:03:33 +0300

    Add files via upload

---
+++ Enum_Kavrami/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Enum_Kavrami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.Pazar);
+            Console.WriteLine((int)Gunler.Cumartesi );
+            
+            int sıcaklık = 32;
+
+            if(sıcaklık <= (int)HavaDurumu.Normal)
+                Console.WriteLine("Dışarıya çıkmak için havanın biraz daha ısınmasını bekle");
+            else if(sıcaklık >= (int)HavaDurumu.Sıcak) 
+                Console.WriteLine("Dışarıya çıkmak için çok sıcak bir gün");
+            else if(sıcaklık >= (int)HavaDurumu.Normal && sıcaklık <(int)HavaDurumu.CokSicak)
+                Console.WriteLine("Hadi Dışarı çıkalım");
+       
+        } 
+    }
+
+    enum Gunler
+    {
+        Pazartesi=1,
+        Salı,
+        Çarşamba,
+        Perşembe,
+        Cuma =23,
+        Cumartesi,
+        Pazar
+
+    }
+    enum HavaDurumu
+    {
+        Soguk = 5,
+        Normal = 20,
+        Sıcak = 25,
+        CokSicak = 30
+
+    }
+
+}
